668f241107210b6ee6da3d5a6f9a35fc7fd61376
khomson kocento <project2you@gmail.com>
2020-08-04 16:21:48 +0700

    Update nginx-php7-2-mysql5-7.sh

--- nginx-php7-2-mysql5-7.sh
+++ nginx-php7-2-mysql5-7.sh
@@ -13,3 +13,4 @@ sudo apt-get install php7.2 -y
 
 # Install needed modules for PHP
 sudo apt-get install php7.2-fpm php7.2-mysql php7.2-curl php7.2-gd php7.2-bz2 php7.2-mbstring php7.2-xml php7.2-zip php-gettext php-xml php7.2-soap  -y
+sudo apt install unzip -y
