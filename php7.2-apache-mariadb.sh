ac374f5177da95408ad46475a32a0d2fb4fc5067
khomson kocento <project2you@gmail.com>
2020-08-04 16:22:32 +0700

    Update php7.2-apache-mariadb.sh

--- php7.2-apache-mariadb.sh
+++ php7.2-apache-mariadb.sh
@@ -15,4 +15,5 @@ sudo apt-get update
 sudo apt-get install php7.2
 
 # Install needed modules for PHP
-sudo apt-get install php7.2-fpm php7.2-mysql php7.2-curl php7.2-gd php7.2-bz2 php7.2-mbstring php7.2-xml php7.2-zip php-gettext php-xml php7.2-soap  -y
+sudo apt-get install php7.2-fpm php7.2-mysql php7.2-curl php7.2-gd php7.2-bz2 php7.2-mbstring php7.2-xml php7.2-zip php-gettext php-xml php7.2-soap -y
+sudo apt install unzip -y
